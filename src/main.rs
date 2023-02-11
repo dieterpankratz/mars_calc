90f4648b15d84e6654be3ce928804919a3768955
dieter <pankratzdieter@gmail.com>
2023-02-11 17:47:27 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut earth_weight = String::new();
+    io::stdin().read_line(&mut earth_weight);
+    let mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {}kg", earth_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
